7e969e79dadb69c97f1c95598440f0fabee22de0
Holly Miranda-Brennecke <loyaltothesport85@gmail.com>
2020-05-11 22:32:26 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,19 @@
-# Write your code here.
+def badge_maker(name)
+  badge_maker = "Hello, my name is #{name}."
+end
+
+
+def batch_badge_creator(array)
+  attendees = []
+  array.each do |badge|
+  attendees << badge_maker(badge)
+  end
+  attendees
+end
+
+
+def assign_rooms(num)
+assign_rooms = attendees
+num
+
+end
