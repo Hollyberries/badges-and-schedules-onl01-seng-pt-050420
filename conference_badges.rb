2d6bd3488ebb76a11ed7e63a7f570ac23352f941
Holly Miranda-Brennecke <loyaltothesport85@gmail.com>
2020-05-11 23:07:09 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -13,7 +13,9 @@ end
 
 
 def assign_rooms(num)
+  speakers = []
   num.each_with_index do |num, index|
+  speakers << assign_rooms(num, index)
  "Hello, #{name}! You'll be assigned to room #{index + 1}!"
   end
 end
